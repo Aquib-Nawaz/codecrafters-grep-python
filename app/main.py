e486a76bd97d8f1931901d203f696369cddffc1e
Aquib Nawaz <aquibnawaz78692@gmail.com>
2024-02-18 20:51:32 +0530

    Stage 2-1

--- app/main.py
+++ app/main.py
@@ -3,17 +3,30 @@ import sys
 # import pyparsing - available if you need it!
 # import lark - available if you need it!
 
+specialCharactersToValueMap = {}
+
+def match_literals(input_line, literal):
+    if len(literal)==1:
+        return literal in input_line
+    literal_values = specialCharactersToValueMap.get(literal, [])
+    for c in literal_values:
+        if c in input_line:
+            return True
+    return False
+
 
 def match_pattern(input_line, pattern):
-    if len(pattern) == 1:
-        return pattern in input_line
-    else:
-        raise RuntimeError(f"Unhandled pattern: {pattern}")
+
+    return match_literals(input_line, pattern)
+    # else:
+    #     raise RuntimeError(f"Unhandled pattern: {pattern}")
 
 
 def main():
     pattern = sys.argv[2]
     input_line = sys.stdin.read()
+
+    specialCharactersToValueMap["\\d"] = [str(i) for i in range(10)]
 
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
